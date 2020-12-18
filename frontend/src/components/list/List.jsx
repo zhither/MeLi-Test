425c8132aff3a57de5c601fd630f925122a4908c
Zhither <m900.eduxa@gmail.com>
2020-12-18 20:25:41 -0300

    MeLi test

--- frontend/src/components/list/List.jsx
+++ frontend/src/components/list/List.jsx
@@ -5,9 +5,10 @@ import { useHistory } from "react-router-dom";
 
 import greenCheck from '../../assets/approval-16.png';
 import back from '../../assets/back.svg';
+import notFound from '../../assets/not-found.svg';
 
 const List = (props) => {
-    
+
     let history = useHistory();
 
     const cardDetail = (id) => {
@@ -16,18 +17,28 @@ const List = (props) => {
     
     return (
         <div className="list">
-            <img src={back} alt="goBack" className='back' onClick={() => history.goBack()}></img>
-            {props.data?.items?.map((item) => (
-                <div key={item.id} className="card" onClick={() => cardDetail(item.id)}>
-                    <div className='img-card'>
-                        <img src={item.picture} />
+            {
+                props.data.items.length === 0 ?
+                    <div className='not-found'>
+                        <img src={notFound}></img>
+                        <h1>No hay resultados que coincidan con su busqueda</h1>
                     </div>
-                    <div className="container">
-                        <h3 className='price'> {item.price.currency} {item.price.amount} {item.free_shipping && <img src={greenCheck}></img>}</h3>
-                        <p className='description'> {item.title} </p>
+                    :
+                    <div>
+                        <img src={back} alt="goBack" className='back' onClick={() => history.goBack()}></img>
+                        {props.data?.items?.map((item) => (
+                            <div key={item.id} className="card" onClick={() => cardDetail(item.id)}>
+                                <div className='img-card'>
+                                    <img src={item.picture} />
+                                </div>
+                                <div className="container">
+                                    <h3 className='price'> {item.price.currency} {item.price.amount} {item.free_shipping && <img src={greenCheck}></img>}</h3>
+                                    <p className='description'> {item.title} </p>
+                                </div>
+                            </div>
+                        ))
+                        }
                     </div>
-                </div>
-            ))
             }
         </div>
     )
